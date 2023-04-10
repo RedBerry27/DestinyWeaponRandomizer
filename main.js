48e2ae9f4611b8c6002443b599ddc2717cb1e4a2
Wh1sper27 <48133917+Wh1sper27@users.noreply.github.com>
2023-04-10 13:46:03 +0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
 function onLoaded(){
+    setTimeout(start,1000);
+}
+
+function start()
+{
     console.log("start2");
     var queryString = window.location.search;
     var urlParams = new URLSearchParams(queryString);
     var code = urlParams.get('code');
+    alert(code);
     if(code == null)
     {
         location.replace("DestinyWeaponRandomizer:0");
