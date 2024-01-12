942b7afb2765a02f8aef9a5118fe3a9e0a5195db
Wh1sper27 <48133917+Wh1sper27@users.noreply.github.com>
2024-01-12 20:47:39 +0500

    Update main.js

--- main.js
+++ main.js
@@ -4,16 +4,17 @@ function onLoaded(){
 
 function start()
 {
-    var queryString = window.location.search;
-    var urlParams = new URLSearchParams(queryString);
-    var code = urlParams.get('code');
-    var state = urlParams.get('state');
-    if(code == null)
-    {
-        location.replace("https://localhost:7186");
-    }
-    else
-    {
-        location.replace("https://localhost:7186/redirect?code=" + code + "&state=" + state);
-    }   
+    
+    //var queryString = window.location.search;
+    //var urlParams = new URLSearchParams(queryString);
+    //var code = urlParams.get('code');
+    //var state = urlParams.get('state');
+    //if(code == null)
+    //{
+        //location.replace("https://localhost:7186");
+    //}
+    //else
+    //{
+        //location.replace("https://localhost:7186/redirect?code=" + code + "&state=" + state);
+    //}   
 }
