2dfb1db46a7dbd925c2cdfc06703d0fe35da4025
Wh1sper27 <deadpie1998@gmail.com>
2024-05-05 15:40:56 +0500

    fetch

--- redirect.js
+++ redirect.js
@@ -2,42 +2,61 @@ var titleElement;
 var localStorage = window.localStorage;
 var tokenEndpoint = "https://www.bungie.net/platform/app/oauth/token/";
 
-function onLoaded(){
+function onLoaded() {
     titleElement = document.getElementById("title");
     setTimeout(start, 1000);
 }
 
-function start()
-{
+function start() {
     var queryString = window.location.search;
     var urlParams = new URLSearchParams(queryString);
     var code = urlParams.get('code');
     var state = urlParams.get('state');
+    let tokenUrl = new URL(tokenEndpoint);
+    tokenUrl.searchParams.append("grant_type", "authorization_code");
+    tokenUrl.searchParams.append("client_id", 46794);
+    tokenUrl.searchParams.append("code", code);
 
-    if(code != null){
-        $.ajax({
-            type: "POST",
-            url: tokenEndpoint + "?grant_type=authorization_code&code=" + code,
-            headers: {'Content-Type': 'application/x-www-form-urlencoded', "Authorization: " : "Basic " + btoa((46794).toString())},
-            dataType: "json",
-            success: function (response) {
-                console.log(response);
-                localStorage.setItem("authorization", JSON.stringify.response);
-                location.replace("/index.html");
+    if (code != null) {
+        let response = fetch(tokenUrl,{
+            method:"POST",
+            headers:{
+                "Content-Type": 'application/x-www-form-urlencoded'
             }
-        });
+        })
+
+        if(response.ok){
+            console.log(response);
+            localStorage.setItem("authorization", JSON.stringify.response);
+            location.replace("/index.html");
+        }
+        else{
+            console.log("ERROR");
+        }
+
+        // $.ajax({
+        //     type: "POST",
+        //     url: tokenUrl,
+        //     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        //     dataType: "json",
+        //     success: function (response) {
+        //         console.log(response);
+        //         localStorage.setItem("authorization", JSON.stringify.response);
+        //         location.replace("/index.html");
+        //     }
+        // });
     }
-    else{
+    else {
         alert("code is null");
     }
 
-    
+
     //if(code == null)
     //{
-        //location.replace("https://localhost:7186");
+    //location.replace("https://localhost:7186");
     //}
     //else
     //{
-        //location.replace("https://localhost:7186/redirect?code=" + code + "&state=" + state);
+    //location.replace("https://localhost:7186/redirect?code=" + code + "&state=" + state);
     //}   
 }
