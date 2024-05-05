2d2f17cc6e240c9d36ed7812eb4bbfb44476ce1d
Wh1sper27 <deadpie1998@gmail.com>
2024-05-05 22:29:29 +0500

    token

--- redirect.js
+++ redirect.js
@@ -21,6 +21,7 @@ function start() {
 
         let response = fetch(tokenUrl,{
             method:"POST",
+            mode:"no-cors",
             headers:{
                 "Content-Type": 'application/x-www-form-urlencoded',
                 "Access-Control-Allow-Origin":"*"
