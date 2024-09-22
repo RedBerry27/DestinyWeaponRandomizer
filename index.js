59094f218853e726942a1bc46870a21b33d848f2
Wh1sper27 <48133917+Wh1sper27@users.noreply.github.com>
2024-09-22 20:00:06 +0500

    Add files via upload
    giga

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 var count = 0;
-var button = document.getElementById("cabbageCount");
-function handleClick(){
-    count++;
-    button.innerHTML = "Всего кочанов: " + count;
+var countElement = document.getElementById("cabbageCount");
+var cabbageButton = document.getElementById("cabbageButton");
+var func;
+var restoreTimeout;
+var normalSize = 300;
+var minSize = 150;
+var stepTap = 10;
+var currentSize = 300;
+var cooldown = 1000;
+
+function handleClick() {
+    
+    
+    if (currentSize > minSize) {
+        count++;
+        currentSize -= stepTap;
+        cabbageButton.style.setProperty("--custom-size", currentSize + "px");
+        if (func != null || func != undefined)
+            clearTimeout(func)
+    
+        func = setTimeout(restoreStepSize, cooldown);
+        countElement.innerHTML = "Всего кочанов: " + count;
+    }    
+}
+
+function toNormalSize() {
+    cabbageButton.style.setProperty("--custom-size", normalSize);
+}
+
+function restoreStepSize() {
+    if (currentSize < normalSize) {
+        currentSize += 10;
+    }
+    cabbageButton.style.setProperty("--custom-size", currentSize + "px");
+    if(currentSize == normalSize){
+
+    }
+    else{
+        func = setTimeout(restoreStepSize, cooldown);
+    }
 }
