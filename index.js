8bd3800a2bd1529bfbbf6a9cd7ac672b1b38b45e
Wh1sper27 <48133917+Wh1sper27@users.noreply.github.com>
2024-09-22 16:48:26 +0500

    Add files via upload
    aaa

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-var titleElement;
-var localStorage = window.localStorage;
-var authorizationEndpoint = "https://www.bungie.net/en/oauth/authorize";
-var tokenEndpoint = "https://www.bungie.net/platform/app/oauth/token/";
-
-function onLoaded(){
-    titleElement = document.getElementById("title");
-    // if(localStorage.getItem("auth"))
-    // {
-    //     localStorage.setItem("auth", JSON.stringify(
-    //     {
-    //         "token":"dfsdgfljl432403df", 
-    //         "id":123299943
-    //     }));
-    // }
-    $("#authButton").click(function (e) { 
-        e.preventDefault();
-        location.replace(authorizationEndpoint + "?client_id=46814&response_type=code");
-    });
-}
-
-function getElement(name){
-    return "<p>" + name + "</p>";
+var count = 0;
+var button = document.getElementById("mainButton");
+function handleClick(){
+    count++;
+    button.innerHTML = "Button " + count;
 }
